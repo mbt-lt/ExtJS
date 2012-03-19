14a6d041102bbcfe43079df823e5a876cbdea835
aur1mas <aur1mas@devnet.lt>
2012-03-19 14:05:57 +0200

    Checkbox field added

---
+++ ExtJS/Form/Element/Checkbox.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ExtJS Form Element Checkbox
+ *
+ * @package ExtJS
+ * @author aur1mas <aur1mas@devnet.lt>
+ */
+class ExtJS_Form_Element_Checkbox extends Zend_Form_Element_Checkbox
+{
+    
+    /**
+     * renders content for form
+     *
+     * @return string
+     * @author aur1mas <aur1mas@devnet.lt>
+     */
+    public function render()
+    {
+        $content = "{
+            xtype: 'checkboxfield',
+            boxLabel: '" . $this->getLabel() . "',
+            name: '" . $this->getName() . "'
+        }";
+        
+        return $content;
+    }
+}
