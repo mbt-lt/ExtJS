da946a87482d5e1913b89d0471046de281bab794
aur1mas <aur1mas@devnet.lt>
2012-03-20 09:38:39 +0200

    allow to add date format

--- ExtJS/Form/Element/DatePicker.php
+++ ExtJS/Form/Element/DatePicker.php
@@ -19,9 +19,21 @@ class ExtJS_Form_Element_DatePicker extends Zend_Form_Element_Text
         $content = "{
             xtype: 'datefield',
             fieldLabel: '" . $this->getLabel() . "',
-            name: '" . $this->getName() . "'
+            name: '" . $this->getName() . "',
+            format: '" . $this->getFormat() . "'
         }";
         
         return $content;
+    }
+    
+    /**
+     * returns date format
+     *
+     * @return string
+     * @author aur1mas <aur1mas@devnet.lt>
+     */
+    public function getFormat()
+    {
+        return $this->getAttrib('format') ? $this->getAttrib('format') : "m/d/Y";
     }
 }
