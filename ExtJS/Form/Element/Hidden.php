be9fe432624c8a193fb77ea6a4481bd2c295b27e
aur1mas <aur1mas@devnet.lt>
2012-03-26 17:03:26 +0300

    hidden element

---
+++ ExtJS/Form/Element/Hidden.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * input type hidden
+ *
+ * @package ExtJS
+ * @author aur1mas <aur1mas@devnet.lt>
+ */
+class ExtJS_Form_Element_Hidden extends Zend_Form_Element_Hidden
+{
+    
+    /**
+     * renders element
+     *
+     * @return string
+     * @author aur1mas <aur1mas@devnet.lt>
+     */
+    public function render()
+    {
+        $content .= "{
+            name: '" . $this->getName() . "',
+            id: '" . $this->getName() ."',
+            xtype: 'hiddenfield',
+            value: '" . $this->getValue() . "'";
+         
+         if ($this->getAttrib('validator')) {
+             $content .= ", validator: " . $this->getAttrib('validator');
+         }
+         
+        if ($this->isRequired()) {
+            $content .= ", allowBlank: false";
+        }
+        
+        $content .= "}";
+
+        
+        return $content;
+    }
+}
